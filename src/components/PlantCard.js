4479767cd64ce82c91c34dc10ad90000841ac348
jobwayne <otienojobwayneotieno@gmail.com>
2024-11-12 15:32:06 +0300

    yes

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -16,3 +16,5 @@ function PlantCard() {
 }
 
 export default PlantCard;
+
+
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,10 +1,24 @@
 import React from "react";
 import PlantCard from "./PlantCard";
-
+const plants = [
+  { id: 1, name: "Aloe Vera", species: "Aloe barbadensis" },
+  { id: 2, name: "Snake Plant", species: "Sansevieria trifasciata" },
+  { id: 3, name: "Peace Lily", species: "Spathiphyllum" },
+  // Add more plants as needed
+];
 function PlantList() {
   return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
+    <ul className="cards">
+      {plants.map((plant) => (
+        <PlantCard key={plants.id} name={plants.name} species={plants.species} />
+      ))}
+    </ul>
   );
 }
 
+ 
+
+
 export default PlantList;
+
+
